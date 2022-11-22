62529a49d84887ba24cf96bd3e76c9ec6da971ab
Fede <federico@freightos.com>
2022-11-22 01:13:09 +0100

    create firs test with the custom commands

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,4 +1,13 @@
 import selectors from '../fixtures/amazonSelectors.json';
+
 describe('First battery of tests', () => {
-    it('Visit a random URL', () => {});
+    it('Visit a random URL', () => {
+        cy.visit('www.amazon.es');
+        cy.clicker(selectors.navSearchBar);
+        cy.typer(selectors.navSearchBar, 'Hola, esto es una prueba');
+        cy.invokeText(selectors.testField);
+        cy.get('@text').then(text => {
+            console.log(text);
+        });
+    });
 });
